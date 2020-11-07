6949e2ffe902d69af770e9e8a5f762412f531297
johanla0 <idlhero@gmail.com>
2020-11-07 21:03:13 +0300

    Update random

--- src/random.js
+++ src/random.js
@@ -1,5 +1,15 @@
-export default (lower, upper) => {
+const getRandomInt = (lower, upper) => {
   const min = Math.ceil(lower);
   const max = Math.floor(upper);
   return Math.floor(Math.random() * (max - min)) + min;
 };
+const generateArrayOfRandoms = (length) => {
+  const lower = 0;
+  const upper = 100;
+  const numbers = [];
+  for (let i = 0; i < length; i += 1) {
+    numbers.push(getRandomInt(lower, upper));
+  }
+  return numbers;
+};
+export { generateArrayOfRandoms, getRandomInt };
