d085a679dabb59965ea8cec7f43ddbe812d41eb3
johanla0 <idlhero@gmail.com>
2020-11-07 15:11:54 +0300

    Move even.js to games/

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import greet from '../src/cli.js';
-import even from '../src/even.js';
+import even from '../games/even.js';
 
 console.log('Welcome to the Brain Games!');
 const name = greet();
