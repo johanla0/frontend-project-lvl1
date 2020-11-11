35f7b7151a892201904e410a9b0d7f6474f7e171
johanla0 <idlhero@gmail.com>
2020-11-11 22:52:15 +0300

    Add default args to random

--- src/random.js
+++ src/random.js
@@ -1,4 +1,4 @@
-const getRandomInt = (lower, upper) => {
+const getRandomInt = (lower = 0, upper = 100) => {
   const min = Math.ceil(lower);
   const max = Math.floor(upper);
   return Math.floor(Math.random() * (max - min)) + min;
