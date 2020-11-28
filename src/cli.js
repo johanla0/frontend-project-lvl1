357ba43685a78d91beefeb1cf4a614821f141b5d
johanla0 <idlhero@gmail.com>
2020-11-28 17:40:06 +0300

    Restored due to hexlet-check requirements

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import promptly from 'promptly';
+
+const greet = async () => {
+  const name = await promptly.prompt('May I have your name? ');
+  console.info(`Hello, ${name}!`);
+  return name;
+};
+
+export default greet;
