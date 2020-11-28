ebaf2444a82264c97c31c74f7b463058554400df
johanla0 <idlhero@gmail.com>
2020-11-28 16:08:42 +0300

    3rd iteration: greet() out of use

--- src/cli.js
+++
@@ -1,9 +0,0 @@
-import promptly from 'promptly';
-
-const greet = async () => {
-  const name = await promptly.prompt('May I have your name? ');
-  console.info(`Hello, ${name}!`);
-  return name;
-};
-
-export default greet;
