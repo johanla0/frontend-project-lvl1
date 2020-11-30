7c7303b8e94d1512cfca5d09c3966e109d831ed0
johanla0 <idlhero@gmail.com>
2020-11-30 16:01:25 +0300

    Fix isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,6 +6,7 @@ const gameParams = {
   range: { min: 0, max: 100 },
 };
 const isPrime = (number) => {
+  if (number < 2) { return false; }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) { return false; }
   }
