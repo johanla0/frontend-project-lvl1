0f0ccf1235e8eb4b4bc056fc7b8178f314b700b8
johanla0 <idlhero@gmail.com>
2020-11-30 09:44:38 +0300

    Move range to gameParams

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,6 +3,7 @@ import start from '../index.js';
 
 const gameParams = {
   rules: 'What is the result of the expression?',
+  range: { min: 0, max: 100 },
   operators: ['+', '-', '*'],
 };
 const calculate = (number1, number2, operator) => {
@@ -20,9 +21,8 @@ const calculate = (number1, number2, operator) => {
 const getOperator = () => gameParams
   .operators[getRandomFromRange(0, gameParams.operators.length - 1)];
 const play = () => {
-  const range = { min: 0, max: 100 };
-  const number1 = getRandomFromRange(range.min, range.max);
-  const number2 = getRandomFromRange(range.min, range.max);
+  const number1 = getRandomFromRange(gameParams.range.min, gameParams.range.max);
+  const number2 = getRandomFromRange(gameParams.range.min, gameParams.range.max);
   const operator = getOperator();
   const question = `Question: ${number1} ${operator} ${number2}`;
   try {
