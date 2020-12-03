fcf83a71bdc5e51ce693c82dc2c7fda625536b63
johanla0 <idlhero@gmail.com>
2020-12-03 17:25:29 +0300

    Remove try-catch

--- src/index.js
+++ src/index.js
@@ -6,21 +6,14 @@ const start = async (rules, getQuestionAndAnswer) => {
   let numberOfTrials = 3;
   console.info(rules);
   while (numberOfTrials > 0) {
-    const questionAndAnswer = {};
-    try {
-      const temp = getQuestionAndAnswer();
-      questionAndAnswer.question = temp.question;
-      questionAndAnswer.correctAnswer = temp.correctAnswer;
-    } catch (e) {
-      console.log(e.message);
-      return;
-    }
-    console.info(`Question: ${questionAndAnswer.question}`);
+    const { question, correctAnswer } = getQuestionAndAnswer();
+
+    console.info(`Question: ${question}`);
     const answer = await promptly.prompt('Your answer: ');
-    if (answer === questionAndAnswer.correctAnswer) {
+    if (answer === correctAnswer) {
       console.info('Correct!');
     } else {
-      console.info(`"${answer}" is wrong answer ;(. Correct answer was "${questionAndAnswer.correctAnswer}".`);
+      console.info(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
       console.info(`Let's try again, ${name}!`);
       return;
     }
