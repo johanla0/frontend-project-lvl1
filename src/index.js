8beeac9bf75d468c4cedb87776b126358b247d52
johanla0 <idlhero@gmail.com>
2020-12-03 17:11:24 +0300

    Add try-catch, get rid of the end check

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
 import promptly from 'promptly';
 
-const start = async (rules, play) => {
+const start = async (rules, getQuestionAndAnswer) => {
   const name = await promptly.prompt('May I have your name? ');
   console.info(`Hello, ${name}!`);
   let numberOfTrials = 3;
   console.info(rules);
   while (numberOfTrials > 0) {
-    const { question, correctAnswer } = play();
-    console.info(question);
+    const questionAndAnswer = {};
+    try {
+      const temp = getQuestionAndAnswer();
+      questionAndAnswer.question = temp.question;
+      questionAndAnswer.correctAnswer = temp.correctAnswer;
+    } catch (e) {
+      console.log(e.message);
+      return;
+    }
+    console.info(`Question: ${questionAndAnswer.question}`);
     const answer = await promptly.prompt('Your answer: ');
-    if (answer === correctAnswer) {
+    if (answer === questionAndAnswer.correctAnswer) {
       console.info('Correct!');
     } else {
-      console.info(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-      break;
+      console.info(`"${answer}" is wrong answer ;(. Correct answer was "${questionAndAnswer.correctAnswer}".`);
+      console.info(`Let's try again, ${name}!`);
+      return;
     }
+
     numberOfTrials -= 1;
   }
-
-  if (numberOfTrials === 0) {
-    console.info(`Congratulations, ${name}!`);
-  } else {
-    console.info(`Let's try again, ${name}!`);
-  }
+  console.info(`Congratulations, ${name}!`);
 };
 
 export default start;
